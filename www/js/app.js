eff4d718e8136e20c1a194cac93279a12688f8fa
Andreas Altherr <andreas.altherr@live.de>
2014-10-01 21:36:51 +0200

    bug fixes, domain von stage auf www. gestellt.

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,7 @@
 
 // default values
 //var agendaServiceUrl = 'http://localhost:64770/umbraco/api/';
-var agendaServiceUrl = 'http://stage.raumstation-club.ch/umbraco/api/';
+var agendaServiceUrl = 'http://www.raumstation-club.ch/umbraco/api/';
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
